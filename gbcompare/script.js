79664a52906357ecaca5ea7283ee88ee327cccbf
CurlyWorm <97799785+CurlyWorm@users.noreply.github.com>
2024-03-11 01:29:50 +0100

    Update script.js
    cleanup

--- gbcompare/script.js
+++ gbcompare/script.js
@@ -1,8 +1,6 @@
 var output = document.getElementById("output");
 var button = document.getElementById("btn");
 // i don't know why i do stuff in the way i do
-console.log(items)
-
 button.addEventListener('click', () => {
     let arr1 = JSON.parse($('#input1').val());
     let arr2 = JSON.parse($('#input2').val());
